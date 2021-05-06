4857681fc46cd3ab5629b3afae6cd39c2b7bc917
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-05-05 23:21:05 -0500

    added mutations resolvers updated app.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        decription: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookId: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
